952697e34a4c9e3c05927dd20c98c5f13e0fbc59
lucas-aquino <luckasakino@protonmail.com>
2023-06-26 09:58:50 -0300

    Se agrego el form principal para test

---
+++ src/components/Form.jsx
@@ -0,0 +1,17 @@
+
+
+
+export default function Form() {
+  return (
+    <div>
+      <form>
+        <div>
+          <input name='nombre'/>
+          <input type='contraseña'/>
+        </div>
+        <input type='submit' value='Enviar'/>
+      </form>
+    </div>
+  )
+}
+
